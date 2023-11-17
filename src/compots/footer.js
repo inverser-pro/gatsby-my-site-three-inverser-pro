b65e10aa3e5a4d585c5499df9d9915c5d8ae1caa
inverser-pro <go@inverser.pro>
2023-11-17 17:07:35 +0300

    Add link to source code

--- src/compots/footer.js
+++ src/compots/footer.js
@@ -4,7 +4,7 @@ const date_now=new Date().getFullYear()
 
 const Footer = ()=>{
     return<footer>
-        copyleft {date_now}. we use: GatsbyJS, React, Google Analytics.
+        copyleft {date_now}. we use: GatsbyJS, React, Google Analytics. <a href="https://github.com/inverser-pro/gatsby-my-site-three-inverser-pro" rel="noopener" target="_blank">Source code</a>
     </footer>;
 }
 
