33ea97a12c16d08e59377c557e62464b53d3f3f9
Inverser Pro <go@inverser.pro>
2024-08-16 22:26:08 +0300

    Add 4/0—4/3, Change in native js (loading YT)

--- src/native-js/js.js
+++ src/native-js/js.js
@@ -4,27 +4,30 @@ const js = ()=>{
     div.classList.add('fx','ac','jc','cp','my2','imgYTPrev');
     setTimeout(() => {
         // <iframe width="560" height="360" title="#0 Welcome in Three js" loading="lazy" src="https://www.youtube.com/embed/" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        const allYT=d_.querySelector('.loadYoutube'); // only 1 YT on page
-        // console.log(allYT,'9999999999');
-        if(allYT){
+        const allYT=d_.querySelectorAll('.loadYoutube'); // only 1 YT on page
+        if(allYT instanceof NodeList){
             // https://i3.ytimg.com/vi/erLk59H86ww/maxresdefault.jpg
-            if(allYT.dataset&&allYT.dataset.id){
-                div.innerHTML=`<img width="1280" height="720" loading="lazy" src="https://i3.ytimg.com/vi/${allYT.dataset.id}/maxresdefault.jpg" alt>`;
-                allYT.after(div);
-                allYT.remove();
-                div.addEventListener('click',()=>{
-                    const divH=div.getBoundingClientRect().height;
-                    const ifr=d_.createElement('iframe');
-                    ifr.width=560;
-                    ifr.height=divH||360;
-                    ifr.src=`https://www.youtube.com/embed/${allYT.dataset.id}?autoplay=1`;
-                    ifr.allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                    ifr.setAttribute('allowfullscreen','');
-                    div.insertAdjacentElement('beforebegin', ifr/* `<iframe width="560" height="360" src="https://www.youtube.com/embed/${allYT.dataset.id}" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <style>imgYTPrev:after{display:none}</style>` */);
-                    div.remove();
-                },true)
-            }
+            allYT.forEach(e=>{
+                if(e.dataset&&e.dataset.id){
+                    const divCln=div.cloneNode(true);
+                    divCln.innerHTML=`<img width="1280" height="720" loading="lazy" src="https://i3.ytimg.com/vi/${e.dataset.id}/maxresdefault.jpg" alt="YouTube Image">`;
+                    e.after(divCln);
+                    e.remove();
+                    divCln.addEventListener('click',()=>{
+                        const divH=div.getBoundingClientRect().height
+                            , ifr=d_.createElement('iframe')
+                        ;
+                        ifr.width=560;
+                        ifr.height=divH||360;
+                        ifr.src=`https://www.youtube.com/embed/${e.dataset.id}?autoplay=1`;
+                        ifr.allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                        ifr.setAttribute('allowfullscreen','');
+                        divCln.insertAdjacentElement('beforebegin', ifr/* `<iframe width="560" height="360" src="https://www.youtube.com/embed/${e.dataset.id}" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <style>imgYTPrev:after{display:none}</style>` */);
+                        divCln.remove();
+                    },true)
+                }
+            })
             const liAll=document.querySelectorAll('aside ul li a');
             //console.log(divH);
             if(liAll.length>0){
